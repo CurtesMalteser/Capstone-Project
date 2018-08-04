ee111779e51a1feae57bf4a4af4bdec0e7ce60a4
CurtesMalteser <antonio_bastiao@hotmail.com>
2018-08-04 18:23:22 +0200

    Raw PoinzWidgetItem, PoinzWidgetService classes

---
+++ PingPoinz/app/src/main/java/com/curtesmalteser/pingpoinz/widget/PoinzWidgetItem.java
@@ -0,0 +1,11 @@
+package com.curtesmalteser.pingpoinz.widget;
+
+/**
+ * Created by António "Curtes Malteser" Bastião on 04/08/2018.
+ */
+public class PoinzWidgetItem {
+    public String text;
+    public PoinzWidgetItem(String text) {
+        this.text = text;
+    }
+}
