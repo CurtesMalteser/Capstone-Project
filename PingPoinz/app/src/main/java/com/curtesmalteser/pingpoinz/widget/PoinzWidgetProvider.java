913994f470c3d5bd4d853d64b0dbe6cb22c0d048
CurtesMalteser <antonio_bastiao@hotmail.com>
2018-08-04 17:48:29 +0200

    Added PoinzWidgetProvider

---
+++ PingPoinz/app/src/main/java/com/curtesmalteser/pingpoinz/widget/PoinzWidgetProvider.java
@@ -0,0 +1,9 @@
+package com.curtesmalteser.pingpoinz.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by António "Curtes Malteser" Bastião on 04/08/2018.
+ */
+public class PoinzWidgetProvider extends AppWidgetProvider {
+}
